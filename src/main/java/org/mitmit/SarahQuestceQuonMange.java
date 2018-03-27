86349cfb6058628ae049165d75f8007931be3cbd
Sarah MITTARD Ext <s.mittard.ext@viaccess-orca.com>
2018-03-27 17:00:16 +0200

    init bootstrap project

---
+++ src/main/java/org/mitmit/SarahQuestceQuonMange.java
@@ -0,0 +1,13 @@
+package org.mitmit;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+
+@SpringBootApplication
+public class SarahQuestceQuonMange {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(SarahQuestceQuonMange.class, args);
+    }
+
+}
