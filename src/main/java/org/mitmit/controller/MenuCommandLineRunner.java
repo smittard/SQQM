1840d6ebb55e34c79785bfcc79ad4c1fe7665ec9
Sarah MITTARD Ext <s.mittard.ext@viaccess-orca.com>
2018-04-08 16:05:45 +0200

    reformat commandLineRunner

--- src/main/java/org/mitmit/controller/MenuCommandLineRunner.java
+++ src/main/java/org/mitmit/controller/MenuCommandLineRunner.java
@@ -1,9 +1,20 @@
-package org.mitmit.services;
+// Copyright (c) 2000-2016 Viaccess SA.
+// Les Collines de l Arche - Tour Opera C, 92057 Paris La Defense, FRANCE
+// All rights reserved.
+//
+// This software is the confidential and proprietary information of
+// Viaccess SA. (Confidential Information). You shall not
+// disclose such Confidential Information and shall use it only in
+// accordance with the terms of the license agreement you entered into
+// with Viaccess.
+//
+package org.mitmit.controller;
 
-import org.mitmit.business.RecipeWorker;
+import org.mitmit.service.RecipeService;
 import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -13,15 +24,19 @@ import static org.slf4j.LoggerFactory.getLogger;
 /**
  * @author mittard.
  */
-@Component
-public class Process implements CommandLineRunner {
+@Service
+public class MenuCommandLineRunner implements CommandLineRunner {
     ///////////////////////////////////////////////////////////////////////////////
     //  Properties
     ///////////////////////////////////////////////////////////////////////////////
 
     //--------------------------------
     // Static properties
-    private static final Logger LOG = getLogger(Process.class);
+    //private static final Logger MSGLOG = getLogger("application");
+    private static final Logger LOG = getLogger(MenuCommandLineRunner.class);
+
+    @Autowired
+    private RecipeService recipeService;
 
     //--------------------------------
     // protected properties
@@ -33,10 +48,6 @@ public class Process implements CommandLineRunner {
     //  Constructors
     ///////////////////////////////////////////////////////////////////////////////
 
-    public Process() {
-        super();
-    }
-
     ///////////////////////////////////////////////////////////////////////////////
     //  Methods
     ///////////////////////////////////////////////////////////////////////////////
@@ -44,9 +55,9 @@ public class Process implements CommandLineRunner {
     //--------------------------------
     // public methods
     @Override
-    public void run(String... args) throws Exception {
-        System.out.println("Sarah Qu'est ce Qu'on Mange ?");
-        System.out.println("Générer le menu de la semaine? (y/n)");
+    public void run(String... args){
+        LOG.warn("Sarah Qu'est ce Qu'on Mange ?");
+        LOG.warn("Générer le menu de la semaine? (y/n)");
         Scanner sc = new Scanner(System.in);
         String input = "n";
         try {
@@ -55,20 +66,16 @@ public class Process implements CommandLineRunner {
             LOG.error("y pour yes ou n pour no ! C'est pas compliqué pourtant !");
         }
         if ("y".equals(input)) {
-            System.out.println("C'est parti !!!");
-            this.getWorker();
+            LOG.debug("Calling algorithm");
+            recipeService.generateMenu();
         }
     }
-
 
     //--------------------------------
     // protected methods
 
     //--------------------------------
     // private methods
-    private Runnable getWorker() {
-        return new RecipeWorker();
-    }
 
     //--------------------------------
     // Getters & Setters
